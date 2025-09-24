c32864d99703537b5f7aa96678fb6848c04730d2
Andres Silva <andressilva1301@gmail.com>
2025-09-24 17:20:05 -0500

    Agregando components ProximosEvents and EvenComponent

---
+++ app/ui/dashboard/NavBarMobile.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+  SportsEsportsRounded,
+  SavingsRounded,
+  MaleRounded,
+  ScaleRounded,
+  MedicalServicesRounded,
+  StrollerRounded,
+  ShuffleRounded,
+} from "@mui/icons-material";
+
+import ItemNav from "../utils/ItemNav";
+
+const NavBarMobile = () => {
+  return (
+    <nav className="max-w-(--widthApp) mx-auto bg-secondary rounded-sm md:hidden">
+      <ul className="flex flex-col justify-around">
+        <ItemNav
+          label="Dashboard"
+          path="/dashboard"
+          icon={<SportsEsportsRounded />}
+        />
+
+        <ItemNav
+          label="Reproductoras"
+          path="/dashboard/reproductoras"
+          icon={<SavingsRounded />}
+        />
+
+        <ItemNav
+          label="Sementales"
+          path="/dashboard/sementales"
+          icon={<MaleRounded />}
+        />
+
+        <ItemNav
+          label="Lechones"
+          path="/dashboard/lechones"
+          icon={<StrollerRounded />}
+        />
+
+        <ItemNav
+          label="Engorde"
+          path="/dashboard/engorde"
+          icon={<ScaleRounded />}
+        />
+
+        <ItemNav
+          label="Veterinaria"
+          path="/dashboard/veterinaria"
+          icon={<MedicalServicesRounded />}
+        />
+
+        <ItemNav
+          label="Movimientos"
+          path="/dashboard/movimientos"
+          icon={<ShuffleRounded />}
+        />
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBarMobile;
